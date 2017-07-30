3dbf3f8759fa52cc08d485c25b4f18c94c070465
Jay Harris <jay@aranasoft.com>
2017-07-29 21:21:53 -0700

    feat(actions): revise Resource and Progress to allow custom methods
    resourse an progress use custom-defined action methods, building object
    methods based on actions, similar to how ngResource works. This allows
    extending resource and progress to using non-default method names for
    restful and progress functionality.
    
    BREAKING CHANGE:
    orbRestfulService and orbRestfykOrigressService no longer accept a
    single orbRestfulResource argument; they now accept [url, paramDefaults,
    actions, options], identical to ngResource.
    
    BREAKING CHANGE:
    resource and progress now use default method names [query, get, create,
    update, delete], rather than the old hard-coded method names [all, find,
    create, update, delete]. These are based on actions passed into the
    module combined with the default actions of the $resourceProvider. To
    continue using [all, find, create, update, delete], specify the actions
    on the resource or update the configuration for $resourceProvider.
    
    closes #1
    closes #2

--- src/orbweaver-progress.js
+++ src/orbweaver-progress.js
@@ -3,59 +3,63 @@
 
   var orbweaver = angular.module('orbProgress', ['orbResource']);
 
-  orbweaver.factory("orbRestfulProgressService", ['$q', 'orbProgressService', function ($q, orbProgressService) {
-    return function (RestfulResource) {
-      var idProperty = RestfulResource.idProperty;
-
-      return {
-        empty: function () {
-          return new RestfulResource();
-        },
-        all: function (params) {
-          return RestfulResource.query(params).$promise;
-        },
-        find: function (id, params) {
-          params = params || {};
-          var options = {};
-          options[idProperty] = id;
-          params = angular.extend(params, options);
-          orbProgressService.start();
-          return RestfulResource.get(params).$promise.finally(function() {
-            orbProgressService.done();
-          });
-        },
-        save: function (inst, params) {
-          params = params || {};
-          if (inst[idProperty]) {
-            var options = {};
-            options[idProperty] = inst[idProperty];
-            params = angular.extend(params, options);
-            orbProgressService.start();
-            return RestfulResource.update(params, inst).$promise.finally(function() {
-              orbProgressService.done();
-            });
-          } else {
-            orbProgressService.start();
-            return RestfulResource.create(params, inst).$promise.finally(function() {
-              orbProgressService.done();
-            });
-          }
-        },
-        'delete': function (inst, params) {
-          params = params || {};
-          var options = {};
-          options[idProperty] = inst[idProperty];
-          params = angular.extend(params, options);
-          orbProgressService.start();
-          return RestfulResource['delete'](params).$promise.finally(function() {
-            orbProgressService.done();
-          });
-        }
-      };
-    };
+  orbweaver.factory('NProgress', ['$window', function($window) {
+    return $window.NProgress;
   }]);
 
-  orbweaver.factory("orbProgressService", function () {
+  orbweaver.provider('orbRestfulProgressService', ['$resourceProvider', 'orbRestfulResourceProvider', function RestfulProgressServiceProvider($resourceProvider, orbRestfulResourceProvider) {
+    this.$get = ['$q', 'orbProgressService', 'orbRestfulService', function ($q, orbProgressService, orbRestfulService) {
+      function restfulProgressServiceFactory(url, params, actions, options) {
+        var extend = angular.extend,
+          forEach = angular.forEach;
+
+        actions = extend({}, $resourceProvider.defaults.actions, orbRestfulResourceProvider.defaults.actions, actions);
+        options = extend({}, orbRestfulResourceProvider.defaults.options, options);
+
+        var resource = new orbRestfulService(url, params, actions, options);
+
+        function RestfulProgressService() {
+          return new resource();
+        }
+
+        RestfulProgressService.empty = function() {
+          return new resource();
+        };
+
+        if (options.saveAsCreateOrUpdate !== false) {
+          RestfulProgressService.save = function (args) {
+            orbProgressService.start();
+            var result = resource.save.apply(this, arguments);
+            if (result.$promise) {
+              return result.$promise.finally(function() { orbProgressService.done(); });
+            } else {
+              orbProgressService.done();
+              return result;
+            }
+          };
+        }
+
+        forEach(actions, function(action, name) {
+          RestfulProgressService[name] = function(args) {
+            orbProgressService.start();
+            var result = resource[name].apply(this, arguments);
+            if (result.$promise) {
+              return result.$promise.finally(function() { orbProgressService.done(); });
+            } else {
+              orbProgressService.done();
+              return result;
+            }
+          };
+        });
+
+        return RestfulProgressService;
+      }
+
+      return restfulProgressServiceFactory;
+    }];
+  }]);
+
+  orbweaver.factory("orbProgressService", ['NProgress', function (NProgress) {
     var progressCounter = 0;
 
     function incrementProgressCounter() {
@@ -73,14 +77,10 @@
     }
 
     return {
-      start: function () {
-        incrementProgressCounter();
-      },
-      done: function () {
-        decrementProgressCounter();
-      }
+      start: incrementProgressCounter,
+      done: decrementProgressCounter
     };
-  });
+  }]);
 
 })(window, window.angular);
 
--- src/orbweaver-resource.js
+++ src/orbweaver-resource.js
@@ -1,72 +1,257 @@
 (function (window, angular, undefined) {
   'use strict';
 
+  var orbResourceMinErr = angular.$$minErr('orbResource');
   var orbweaver = angular.module('orbResource', ['ngResource']);
 
-  orbweaver.factory("orbRestfulResource", ['$resource', function ($resource) {
-    return function (url, params, methods, options) {
-      var idProperty = 'id';
-      if( options && options.idProperty ) { idProperty = options.idProperty; }
+  orbweaver.provider('orbRestfulResource', function RestfulResourceProvider() {
+    var provider = this;
 
-      var defaults = {
-        update: {method: 'put', isArray: false},
-        create: {method: 'post'}
-      };
-
-      methods = angular.extend(defaults, methods);
-
-      var resource = $resource(url, params, methods);
-      resource.idProperty = idProperty;
-
-      resource.prototype.$save = function (params, success, failure) {
-        if (!this[idProperty]) {
-          this.$create(params, success, failure);
-        } else {
-          this.$update(params, success, failure);
-        }
-      };
-
-      return resource;
+    this.defaults = {
+      options: {
+        idProperty: 'id',
+        saveAsCreateOrUpdate: true
+      },
+      actions: {
+        update: {method: 'PUT'},
+        create: {method: 'POST'}
+      }
     };
-  }]);
 
-  orbweaver.factory("orbRestfulService", ['$q', function ($q) {
-    return function (RestfulResource) {
-      var idProperty = RestfulResource.idProperty;
+    this.$get = ['$resource', function ($resource) {
+      function restfulResourceFactory (url, params, actions, options) {
+        var extend = angular.extend,
+          isFunction = angular.isFunction;
 
-      return {
-        empty: function () {
-          return new RestfulResource();
-        },
-        all: function (params) {
-          return RestfulResource.query(params).$promise;
-        },
-        find: function (id, params) {
-          params = params || {};
-          var options = {};
-          options[idProperty] = id;
-          params = angular.extend(params, options);
-          return RestfulResource.get(params).$promise;
-        },
-        save: function (inst, params) {
-          params = params || {};
-          if (inst.id) {
-            var options = {};
-            options[idProperty] = inst[idProperty];
-            params = angular.extend(params, options);
-            return RestfulResource.update(params, inst).$promise;
-          } else {
-            return RestfulResource.create(params, inst).$promise;
-          }
-        },
-        'delete': function (inst, params) {
-          params = params || {};
-          var options = {};
-          options[idProperty] = inst[idProperty];
-          params = angular.extend(params, options);
-          return RestfulResource['delete'](params).$promise;
+        actions = extend({}, provider.defaults.actions, actions);
+        options = extend({}, provider.defaults.options, options);
+
+        var resource = $resource(url, params, actions, options);
+        resource.idProperty = options.idProperty;
+
+        if (options.saveAsCreateOrUpdate !== false) {
+          resource.prototype.$save = function (params, success, error) {
+            if (isFunction(params)) {
+              error = success; success = params; params = {};
+            }
+            var result = resource.save.call(this, params, this, success, error);
+            return result.$promise || result;
+          };
+          resource.save = function(a1, a2, a3, a4) {
+            var params = {}, data, success, error;
+
+            switch (arguments.length) {
+              case 4:
+                error = a4;
+                success = a3;
+                /* falls through */
+              case 3:
+              case 2:
+                if (isFunction(a2)) {
+                  if (isFunction(a1)) {
+                    throw orbResourceMinErr('badargs',
+                      'Expected between first argument to not be a function [[params,] data, [success [, error]]]');
+                  }
+
+                  success = a2;
+                  error = a3;
+                } else {
+                  params = a1;
+                  data = a2;
+                  success = a3;
+                  break;
+                }
+                /* falls through */
+              case 1:
+                data = a1;
+                break;
+              default:
+                throw orbResourceMinErr('badargs',
+                  'Expected between 1 and 4 arguments [[params,] data, [success [, error]]], got {0} arguments',
+                  arguments.length);
+            }
+
+            var method;
+            if (!data[this.idProperty]) {
+              method = 'create';
+            } else {
+              method = 'update';
+            }
+            return resource[method].call(this, params, data, success, error);
+          };
         }
-      };
-    };
+
+        return resource;
+      }
+
+      return restfulResourceFactory;
+    }];
+  });
+
+  orbweaver.provider('orbRestfulService', ['$resourceProvider', 'orbRestfulResourceProvider', function RestfulServiceProvider($resourceProvider, orbRestfulResourceProvider) {
+    this.$get = ['$q', 'orbRestfulResource', function ($q, orbRestfulResource) {
+      function restfulServiceFactory(url, params, actions, options) {
+        var extend = angular.extend,
+          forEach = angular.forEach,
+          isDefined = angular.isDefined,
+          isFunction = angular.isFunction,
+          isObject = angular.isObject;
+
+        actions = extend({}, $resourceProvider.defaults.actions, orbRestfulResourceProvider.defaults.actions, actions);
+        options = extend({}, orbRestfulResourceProvider.defaults.options, options);
+
+        var resource = new orbRestfulResource(url, params, actions, options);
+
+        function RestfulService() {
+          return new resource();
+        }
+
+        RestfulService.empty = function() {
+          return new resource();
+        };
+
+        if (options.saveAsCreateOrUpdate !== false) {
+          RestfulService.save = function (arg1, arg2, arg3, arg4) {
+            var params = {}, data, success, error;
+
+            switch (arguments.length) {
+              case 4:
+                error = arg4;
+                success = arg3;
+                /* falls through */
+              case 3:
+              case 2:
+                if (isFunction(arg2)) {
+                  if (isFunction(arg1)) {
+                    throw orbResourceMinErr('badargs',
+                      'Expected first argument to not be a function [[params,] data, [success [, error]]]');
+                  }
+
+                  data = arg1;
+                  success = arg2;
+                  error = arg3;
+                } else {
+                  params = arg1;
+                  data = arg2;
+                  success = arg3;
+                }
+                break;
+              case 1:
+                if (isFunction(arg1)) {
+                  throw orbResourceMinErr('badargs',
+                    'Expected first argument to not be a function [[params,] data, [success [, error]]]');
+                }
+                data = arg1;
+                break;
+              default:
+                throw orbResourceMinErr('badargs',
+                  'Expected between 1 and 4 arguments [[params,] data, [success [, error]]], got {0} arguments',
+                  arguments.length);
+            }
+
+            var result;
+            if (isDefined(data[options.idProperty])) {
+              result = resource.update.apply(this, arguments);
+            } else {
+              result = resource.create.apply(this, arguments);
+            }
+            return result.$promise || result;
+
+          };
+        }
+
+        forEach(actions, function(action, name) {
+          var hasBody = action.hasBody === true || (action.hasBody !== false && /^(POST|PUT|PATCH)$/i.test(action.method));
+
+          RestfulService[name] = function(arg1, arg2, arg3, arg4, arg5) {
+            var identity, params = {}, data, success, error;
+
+            switch (arguments.length) {
+              case 5:
+              case 4:
+                if (isFunction(arg3)) {
+                  success = arg3;
+                  error = arg4;
+                } else {
+                  success = arg4;
+                  error = arg5;
+                }
+                /* falls through */
+              case 3:
+                if (isFunction(arg2)) {
+                  error = arg3;
+                  success = arg2;
+                  data = arg1;
+                } else if (isFunction(arg3)) {
+                  error = arg4;
+                  success = arg3;
+                  data = arg2;
+                  if (isObject(arg1)) {
+                    params = arg1;
+                  } else {
+                    identity = arg1;
+                  }
+                } else {
+                  data = arg3;
+                  params = arg2;
+                  identity = arg1;
+                }
+                break;
+              case 2:
+                if (isFunction(arg2)) {
+                  if (isFunction(arg1)) {
+                    error = arg2;
+                  } else {
+                    success = arg2;
+                  }
+                } else {
+                  if (hasBody) {
+                    data = arg2;
+                    if (isObject(arg1)) {
+                      params = arg1;
+                    } else {
+                      identity = arg1;
+                    }
+                  } else {
+                    params = arg2;
+                    identity = arg1;
+                  }
+                  break;
+                }
+                /* falls through */
+              case 1:
+                if (isFunction(arg1)) {
+                  success = arg1;
+                } else if (hasBody) {
+                  data = arg1;
+                } else if (isObject(arg1)) {
+                  params = arg1;
+                } else {
+                  identity = arg1;
+                }
+                break;
+              case 0: break;
+              default:
+                throw orbResourceMinErr('badargs',
+                  'Expected between 1 and 5 arguments [[id,] [params,] data, [success [, error]]], got {0} arguments',
+                  arguments.length);
+            }
+
+            if (isDefined(identity)) {
+              var identityParams = {};
+              identityParams[options.idProperty] = identity;
+              params = extend({}, params, identityParams);
+            }
+
+            var result = resource[name](params, data, success, error);
+            return result.$promise || result;
+          };
+        });
+
+        return RestfulService;
+      }
+
+      return restfulServiceFactory;
+    }];
   }]);
 })(window, window.angular);
